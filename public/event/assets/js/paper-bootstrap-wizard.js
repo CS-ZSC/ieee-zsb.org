5a6e9840cb8337c3bb59ddd6c62aa0aa1d687f1a
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-01-28 00:26:37 +0200

    Trying radio button fix

--- public/event/assets/js/paper-bootstrap-wizard.js
+++ public/event/assets/js/paper-bootstrap-wizard.js
@@ -131,7 +131,7 @@ transparent = true;
                     wizard.find('[data-toggle="wizard-radio"]').removeClass('active');
                     $(this).addClass('active');
                     $(wizard).find('[type="radio"]').removeAttr('checked');
-                    $(this).find('[type="radio"]').attr('checked','true');
+                    $(this).find('[type="radio"]').attr('checked','checked');
                 });
 
                 $('[data-toggle="wizard-checkbox"]').click(function(){
