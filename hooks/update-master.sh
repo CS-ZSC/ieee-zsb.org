eeae3497ac69b5496474cb0c136c48ace522a4b4
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-01-30 20:21:18 +0200

    Removed interpreters

--- hooks/update-master.sh
+++ hooks/update-master.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
 cd $HOME/master && git pull && npm install
 pm2 restart $HOME/ecosystem.config.js --only master
--- hooks/update-testing.sh
+++ hooks/update-testing.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
 cd $HOME/testing && git pull && npm install
 pm2 restart $HOME/ecosystem.config.js --only testing
