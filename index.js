e7781d4889fbb828991c568a63f1fa5c248378a6
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-02-02 16:48:43 +0200

    Fixed relative path in module name

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(expressLogging(logger));
 
 controllers(app); /* Setup routes */
-require('models/webhook')(app); /* Setup webhook */
+require('./models/webhook')(app); /* Setup webhook */
 
 var port = process.env.PORT || 80;
 app.listen(port, function() {
