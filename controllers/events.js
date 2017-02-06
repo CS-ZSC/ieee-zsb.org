11aabb131133c90b92ee5ccf5109c8cdcea75140
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-02-06 19:09:31 +0200

    Started DWH attachment

--- controllers/events.js
+++ controllers/events.js
@@ -5,17 +5,35 @@ router.get('/mutex', function(req, res) {
   res.render('events/mutex');
 });
 
+router.get('/mutex/confirm/:id', function(req, res) {
+  res.send(req.params.id);
+});
+
 router.post('/mutex', function(req, res) {
-  database.insertForm(req.body, function(err, db) {
-    if (err == 'Already registered') {
-      res.render('events/failure', {email: req.body.email});
-    } else if (err) {
-      res.render('events/error');
-    } else {
-      res.render('events/success');
+  var timestamp = +new Date();
+  req.body.timestamp = timestamp;
+  /* Validate required fields */
+  if (req.body.fullname && req.body.email && req.body.level && req.body.workshops) {
+    if (!req.body.question) { /* Bypassed level question */
+      req.body.programmer = 1;
     }
-    db.close();
-  });
+    res.render('events/confirm', {fullname: req.body.fullname, link: 'http://ieee-zsb.org'});
+    /*database.warehouse.insertDoc('mutex_form', 'req_headers', req.headers, function(err) {
+      console.log(err);
+    });*/
+    /*database.insertForm(req.body, function(err, db) {
+      if (err == 'Already registered') {
+        res.render('events/failure', {email: req.body.email});
+      } else if (err) {
+        res.render('events/error');
+      } else {
+        res.render('events/success', {msg: 'Successfully registered for MUTEX event'});
+      }
+      db.close();
+    });*/
+  } else {
+    res.render('events/error', {error: 'ERR::INVALID_REQ'});
+  }
 });
 
 module.exports = router;
--- models/database.js
+++ models/database.js
@@ -1,7 +1,28 @@
 var MongoClient = require('mongodb').MongoClient;
 
 var mongoURI = process.env.MONGO_URI;
+var dataWarehouse = process.env.DWH_URI;
+var dwhDbURI = (db) => dataWarehouse + '/' + db + '?authSource=admin';
 var collection = "testing";
+
+exports.warehouse = {
+  insertDoc: function(db, collection, doc, callback) {
+    MongoClient.connect(dwhDbURI(db), function(err, db) {
+      if (err) {
+        callback(err);
+      } else {
+        db.collection(collection).insertOne(doc, function(err, result) {
+          if (err) {
+            callback(err);
+          } else {
+            callback(null);
+          }
+        });
+        db.close();
+      }
+    });
+  }
+};
 
 exports.insertForm = function(user, callback) {
   MongoClient.connect(mongoURI, function(err, db) {
