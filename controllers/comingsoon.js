b18a9f30d1f2fd09d0253050f2171154a6aff348
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-02-24 16:16:59 +0200

    Test: added fullname to all responses

--- controllers/comingsoon.js
+++ controllers/comingsoon.js
@@ -10,14 +10,14 @@ router.post('/check', function(req, res) {
   if (id && id.match(/^[0-9a-fA-F]{24}$/)) {
     database.getDoc(id, function(err, db, doc) {
       if (err) {
-        res.send({id: id, accepted: false, attended: false, error: err});
+        res.send({id: id, fullname: "", accepted: false, attended: false, error: err});
       } else {
         res.send({id: id, fullname: doc.fullname, accepted: doc.accepted, attended: doc.attended? true : false})
       }
       if (db) db.close();
     })
   } else {
-    res.send({id: id, fullname: undefined, accepted: false, attended: false, error: "INV_ID"});
+    res.send({id: id, fullname: "", accepted: false, attended: false, error: "INV_ID"});
   }
 });
 
