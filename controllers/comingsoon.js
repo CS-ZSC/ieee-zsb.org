d46c7c5124958d6c8e3843ac3b6a269f61708871
Mohamed Anwar <mohamed_anwar@opmbx.org>
2017-02-24 15:16:24 +0200

    Testing checking server

--- controllers/comingsoon.js
+++ controllers/comingsoon.js
@@ -1,7 +1,24 @@
 const router = require('express').Router();
+const database = require('../models/database');
 
 router.get('/', function(req, res) {
   res.render('comingsoon');
+});
+
+router.post('/check', function(req, res) {
+  var id = req.query.id;
+  if (id && id.match(/^[0-9a-fA-F]{24}$/)) {
+    database.getDoc(id, function(err, db, doc) {
+      if (err) {
+        res.send({accepted: false, attended: false, error: err});
+      } else {
+        res.send({fullname: doc.fullname, accepted: doc.accepted, attended: doc.attended? true : false})
+      }
+      if (db) db.close();
+    })
+  } else {
+    res.send({accepted: false, attended: false, error: "INV_ID"});
+  }
 });
 
 module.exports = router;
